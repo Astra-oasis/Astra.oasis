4056afc82c409310f7207bc3bbaa82c0a9471267
DHieu <hyoyoeng2105@gmail.com>
2026-04-07 08:47:50 +0700

    fix frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -182,7 +182,7 @@ export default function Home() {
             <KingOfTheHill coin={topCoinByMarketCap} onClick={handleCoinClick} />
             <div className="mt-8">
               <FilterBar currentSort={sortOption} onSortChange={setSortOption} />
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                 {sortedCoins.map(coin => <CoinCard key={coin.id} coin={coin} onClick={handleCoinClick} />)}
               </div>
             </div>
